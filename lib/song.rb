778fb5f0f72c447fe1009e92e940c93b0e09150e
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-15 19:13:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,7 @@ class Song
     a_count = 1 
     @@artists.each do |a| 
       if artist_count.include?(a)
-        a_count += 1
-        artist_count[a] = a_count
+        artist_count[a] = a_count += 1
       else
         artist_count[a] = a_count
       end
